ea143b4ae2d722cf52572f1da6873c41eca24563
Dmitry Rakovich <dmitryrakovich@mail.ru>
2023-06-05 16:01:24 +0300

    add: pagination

--- src/pages/Favoritespage.jsx
+++ src/pages/Favoritespage.jsx
@@ -2,15 +2,21 @@ import { useEffect, useState } from "react";
 import Jobitem from "../components/Jobitem";
 import Emptypage from "./Emptypage";
 import Loader from "../components/Loader";
+import Pagination from "../components/Pagination";
 
 export default function Favoritespage() {
   const [vacancies, setVacancies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(0);
+  const [total, setTotal] = useState(500);
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || []
 
   useEffect(() => {
     document.title = "Избранное";
-    const paramString =
-      JSON.parse(localStorage.getItem("favorites"))?.join("&ids[]=") || "";
+    const paramString = favorites.join("&ids[]=") + `&${new URLSearchParams ({
+      count: 4,
+      page: page,
+    })}` ;
       const { token_type, access_token } = JSON.parse(
         localStorage.getItem("paralect_token")
       );
@@ -22,24 +28,35 @@ export default function Favoritespage() {
       },
     })
       .then((res) => res.json())
-      .then((data) => setVacancies(data.objects))
+      .then((data) => {
+        setVacancies(data.objects)
+        setTotal(data.total)
+      })
       .then(() => setIsLoading(false));
-  }, []);
+  }, [page]);
 
   return (
     <div className="vacancy_wrapper">
       {isLoading ? (
         <Loader />
       ) : vacancies.length > 0 ? (
-        vacancies.map((vacancy) => (
-          <Jobitem
+        <>
+        {
+
+          vacancies.map((vacancy) => (
+            <Jobitem
             key={vacancy.id}
             vacancy={vacancy}
-          />
-        ))
+            />
+            ))
+          }
+        <Pagination total={total} setPage={setPage} />
+            </>
+        
       ) : (
         <Emptypage />
       )}
+      {/* <Pagination total={total} setPage={setPage} /> */}
     </div>
   );
 }
